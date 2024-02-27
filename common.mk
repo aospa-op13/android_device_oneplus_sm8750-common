44948a50523b71029a46f1079776445cdf02c230
Vishalcj17 <vishalcj@aospa.co>
2024-02-27 14:19:54 +0900

    sm8750-common: Move to vendor overlay component.
    Change-Id: I596f24135ebcc10bb0ba385c91367a160c7bab1e
    Signed-off-by: Fazil Sheik <fazilsheik@aospa.co>

--- common.mk
+++ common.mk
@@ -206,13 +206,11 @@ PRODUCT_PACKAGES += \
     AOSPAOnePlusSM8750Frameworks \
     AOSPAOnePlusSM8750SystemUI \
     FrameworksResTargetEuicc \
-    FrameworksResTarget \
     NcmTetheringOverlay \
     OnePlusSM8750CarrierConfig \
     OnePlusSM8750Frameworks \
     OnePlusSM8750Settings \
-    OnePlusSM8750SystemUI \
-    WifiResTarget
+    OnePlusSM8750SystemUI
 
 # Partitions
 PRODUCT_PACKAGES += \
