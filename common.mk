cfa2a74b38a12cf3bb36fbbbdb92e1be74ad24fa
pjgowtham <pjgowtham@gmail.com>
2024-10-04 19:25:52 +0530

    sm8750-common: Switch to oplus sensors AIDL
    Change-Id: Ibe50667179a42845918f84bc403da21d2c28b160

--- common.mk
+++ common.mk
@@ -213,7 +213,7 @@ PRODUCT_COPY_FILES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors-service.multihal \
+    android.hardware.sensors-service.oplus-multihal \
     sensors.dynamic_sensor_hal \
     sensors.qsh_wrapper
 
