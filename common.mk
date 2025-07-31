a5398b291499a5458b3856fa7ab0096b73bd76b3
Fazil Sheik <fazilsheik@aospa.co>
2025-07-31 23:24:25 +0530

    sm8750-common: Switch to QTI telephony component
    Change-Id: Ifca46276dd9c9b9f8bd8c8bfc1d2d9e763498512
    Signed-off-by: Fazil Sheik <fazilsheik@aospa.co>

--- common.mk
+++ common.mk
@@ -112,12 +112,6 @@ $(call inherit-product, vendor/qcom/opensource/healthd-ext/health-vendor-product
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/permissions/privapp-permissions-hotword.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-hotword.xml
 
-# IPACM
-PRODUCT_PACKAGES += \
-    ipacm \
-    IPACM_cfg.xml \
-    IPACM_Filter_cfg.xml
-
 # IR
 PRODUCT_PACKAGES += \
     android.hardware.ir-service.oplus
@@ -189,6 +183,7 @@ TARGET_COMMON_QTI_COMPONENTS := \
     gps \
     init \
     overlay \
+    telephony \
     wfd
 
 PRODUCT_PACKAGES += \
@@ -259,41 +254,19 @@ PRODUCT_SOONG_NAMESPACES += \
 # Storage
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
-# Telephony
+# Oplus EUICC
 PRODUCT_PACKAGES += \
     OplusEuicc \
-    extphonelib \
-    extphonelib-product \
-    extphonelib.xml \
-    extphonelib_product.xml \
-    ims-ext-common \
-    ims_ext_common.xml \
-    nrmodeswitcher \
-    qti-telephony-hidl-wrapper \
-    qti-telephony-hidl-wrapper-prd \
-    qti_telephony_hidl_wrapper.xml \
-    qti_telephony_hidl_wrapper_prd.xml \
-    qti-telephony-utils \
-    qti-telephony-utils-prd \
-    qti_telephony_utils.xml \
-    qti_telephony_utils_prd.xml \
-    telephony-ext
-
-PRODUCT_BOOT_JARS += \
-    telephony-ext
 
 PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.telephony.cdma.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.cdma.xml \
-    frameworks/native/data/etc/android.hardware.telephony.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.euicc.xml \
-    frameworks/native/data/etc/android.hardware.telephony.gsm.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.gsm.xml \
-    frameworks/native/data/etc/android.hardware.telephony.ims.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.ims.xml \
-    frameworks/native/data/etc/android.hardware.telephony.mbms.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.mbms.xml
+    frameworks/native/data/etc/android.hardware.telephony.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.euicc.xml
 
 # Oplus fwk
 $(call inherit-product, hardware/oplus/oplus-fwk/oplus-fwk.mk)
 
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.sip.voip.xml
+# Telephony
+PRODUCT_PACKAGES += \
+    nrmodeswitcher
 
 # Thermal
 PRODUCT_PACKAGES += \
