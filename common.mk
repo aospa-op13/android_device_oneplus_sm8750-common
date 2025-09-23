6308f54a7c90cd67efe5ce445f8341e297350371
Fazil Sheik <fazilsheik@aospa.co>
2025-09-23 21:31:28 +0530

    sm8750-common: Switch to QTI usb component
    Change-Id: I39e439f79715d04b71b7c60b759fc643f3eb78cf

--- common.mk
+++ common.mk
@@ -178,6 +178,7 @@ TARGET_COMMON_QTI_COMPONENTS := \
     overlay \
     perf \
     telephony \
+    usb \
     wfd \
     wlan
 
@@ -185,7 +186,6 @@ PRODUCT_PACKAGES += \
     AOSPAOnePlusSM8750Frameworks \
     AOSPAOnePlusSM8750SystemUI \
     FrameworksResTargetEuicc \
-    NcmTetheringOverlay \
     OnePlusSM8750CarrierConfig \
     OnePlusSM8750Frameworks \
     OnePlusSM8750Settings \
@@ -278,18 +278,7 @@ PRODUCT_PACKAGES_DEBUG += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb-service.qti \
-    android.hardware.usb.gadget-service.qti \
-    init.qcom.usb.rc \
-    init.qcom.usb.sh \
     oplus_usb_compositions.conf
-
-PRODUCT_SOONG_NAMESPACES += \
-    vendor/qcom/opensource/usb/etc
-
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.usb.accessory.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.accessory.xml \
-    frameworks/native/data/etc/android.hardware.usb.host.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.host.xml
 
 # Verified Boot
 PRODUCT_COPY_FILES += \
