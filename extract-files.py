83de09282f7cd4c1219d0726e1a591422197096e
Fazil Sheik <fazilsheik@aospa.co>
2025-10-03 02:08:24 +0530

    sm8750-common: Switch to QTI media component
    Change-Id: Ia712d30ae19214410f5293dd50cb343b809ad8db

--- extract-files.py
+++ extract-files.py
@@ -26,6 +26,7 @@ namespace_imports = [
     'vendor/qcom/opensource/commonsys-intf/display',
     'vendor/qcom/opensource/dataservices',
     'vendor/qcom/common/vendor/adreno-v',
+    'vendor/qcom/common/vendor/media/6.6',
     'vendor/qcom/common/vendor/perf',
 ]
 
