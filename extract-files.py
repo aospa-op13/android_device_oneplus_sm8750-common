ddf8d7e1087bfa19c1ea05d0b7693bbadf970b6d
Weikai Kong <priv@pppig236.com>
2025-11-02 23:55:48 -0500

    sm8750-common: Drop unnecessary duplicated copies of blobs
    Change-Id: Ib8f3b399d527d62f2b21bee6f80afc3edbc0385c

--- extract-files.py
+++ extract-files.py
@@ -34,9 +34,6 @@ namespace_imports = [
 def lib_fixup_vendor_suffix(lib: str, partition: str, *args, **kwargs):
     return f'{lib}_{partition}' if partition == 'vendor' else None
 
-def lib_fixup_odm_suffix(lib: str, partition: str, *args, **kwargs):
-    return f'{lib}_{partition}' if partition == 'odm' else None
-
 lib_fixups: lib_fixups_user_type = {
     **lib_fixups,
     (
@@ -75,9 +72,6 @@ lib_fixups: lib_fixups_user_type = {
         'vendor.qti.qccsyshal_aidl-V1-ndk',
         'vendor.qti.qccvndhal_aidl-V1-ndk',
     ): lib_fixup_vendor_suffix,
-    (
-        'vendor.oplus.hardware.displaypanelfeature-V1-ndk',
-    ): lib_fixup_odm_suffix,
     (
         'libagm',
         'libagmclient',
