4f7f9a2491a719127631eb822d93c34fb3c9a6d3
Nolen Johnson <johnsonnolen@gmail.com>
2026-06-26 01:19:38 -0400

    sm8750-common: fastbootd is now enabled by default
    Change-Id: Ieb54140550168f84ba4f067605fd6f7107979c31

--- common.mk
+++ common.mk
@@ -111,8 +111,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/generic_ramdisk.mk)
 
 # Fastboot
 PRODUCT_PACKAGES += \
-    android.hardware.fastboot-service.example_recovery \
-    fastbootd
+    android.hardware.fastboot-service.example_recovery
 
 # Fingerprint
 PRODUCT_PACKAGES += \
