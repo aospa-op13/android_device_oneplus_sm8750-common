3c7fa0f03846096119bd871ad7d6782acc7857b9
Fazil Sheik <fazilsheik@aospa.co>
2026-08-06 15:48:33 +0530

    Revert "sm8750-common: Disable virtual A/B compression"
    Change-Id: If9558f41679042999da6ea7947c4778bdf9b51d0

--- common.mk
+++ common.mk
@@ -10,7 +10,8 @@ PRODUCT_USE_SCUDO := true
 ENABLE_AB := true
 ENABLE_VIRTUAL_AB := true
 
-$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/vabc_features.mk)
+PRODUCT_VIRTUAL_AB_COMPRESSION_METHOD := lz4
 
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
