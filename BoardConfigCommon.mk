04881a7906e140fcc4ae13e66e972179b339a196
Fazil Sheik <fazilsheik@aospa.co>
2026-09-13 17:15:49 +0530

    sm8750-common: Enable kernel console for debug
    Change-Id: I776b840a502ea63008c46f51022c3caab308b0ae

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -59,7 +59,7 @@ BOARD_BOOTCONFIG := \
     androidboot.hypervisor.protected_vm.supported=true \
     androidboot.load_modules_parallel=true \
     androidboot.memcg=1 \
-    androidboot.serialconsole=0 \
+    androidboot.serialconsole=1 \
     androidboot.usbcontroller=a600000.dwc3 \
     androidboot.vendor.qspa=true
 
